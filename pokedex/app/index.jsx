b95f440373da63fb5943ef28f515c5f2ca63a6ac
matheusf21 <matheusferreiradelima21@gmail.com>
2025-08-13 13:59:00 -0300

    V0.4

--- pokedex/app/index.jsx
+++ pokedex/app/index.jsx
@@ -1,24 +1,25 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import Header from '@/components/Header';
 import { pokemons } from "@/data/pokemons"
+import Card from "../components/Card";
 
 export default function Index() {
   
     const renderPokemon = ({ item }) => (
-      <Text>{item.Nome}</Text>
+      <Card pokemon={item} />
     )
  
   return (
       <View style={styles.container}>
         <Header title="Pokedéx" />
         <FlatList
-        data={pokemons}
-        style={styles.cards}
-        keyExtractor={(pokemon) => pokemons.Numero.toString()}
-        renderItem={renderPokemon}
-        initiaNumToRender={10}
-        maxtoRenderPerBatch={10}
-        windowSize={5}
+          data={pokemons}
+          style={styles.cards}
+          keyExtractor={(pokemon) => pokemon.Numero.toString()}
+          renderItem={renderPokemon}
+          initiaNumToRender={10}
+          maxtoRenderPerBatch={10}
+          windowSize={5}
         />
       </View>
   );
--- pokedex/components/Card/styles.jsx
+++ pokedex/components/Card/styles.jsx
@@ -1,5 +1,6 @@
 import { StyleSheet } from "react-native";
 
+
 export const styles = StyleSheet.create({
     container: {
         width: '100%',
